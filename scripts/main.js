9e62989baace4ec4d26bead22b0741bb8ebb9e24
sergioIgnatev <sergioignatev17@gmail.com>
2023-03-12 22:52:11 +0300

    h

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,17 @@
-document.querySelector('div').addEventListener('click',
+var cube = document.querySelector('.cube');
+var radioGroup = document.querySelector('.radio-group');
+var currentClass = '';
 
-function(){
-    
-    document.querySelector('div').classList.add('anim');
-    document.querySelector('div').innerHTML=""
-    setTimeout(
-        function(){
-            document.querySelector('.anim').innerHTML=`<i class="fa fa-bolt" aria-hidden="true"></i>`
-            document.querySelector('.anim').style.color='black';
-            document.querySelector('.anim').style.background='white'
-    },5000
-    )
-    setTimeout(
-        function(){
-            document.querySelector('.anim').innerHTML=`<i class="fa fa-battery-quarter" aria-hidden="true"></i>`
-        },10000
-    )
+function changeSide() {
+  var checkedRadio = radioGroup.querySelector(':checked');
+  var showClass = 'show-' + checkedRadio.value;
+  if ( currentClass ) {
+    cube.classList.remove( currentClass );
+  }
+  cube.classList.add( showClass );
+  currentClass = showClass;
 }
+// set initial side
+changeSide();
 
-)
+radioGroup.addEventListener( 'change', changeSide );
